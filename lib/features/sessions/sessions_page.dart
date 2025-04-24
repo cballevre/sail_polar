1add9d04c0d4ef8e57d75ee8c804d156dd2389c6
cballevre <celestin.ballevre@protonmail.com>
2025-04-24 08:10:20 +0200

    feat: Add 3 main page

---
+++ lib/features/sessions/sessions_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SessionsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Sessions Page'),
+    );
+  }
+}
